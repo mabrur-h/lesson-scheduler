2019d30adb0d7c090f540b930403e57ec6f5b840
Mabrur <mabrur.h11@gmail.com>
2022-04-21 09:40:34 +0500

    update student getter

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import {Args, Mutation, Resolver} from "@nestjs/graphql";
+import {Args, Mutation, Query, Resolver} from "@nestjs/graphql";
 import {StudentType} from "./student.type";
 import {StudentService} from "./student.service";
 import {CreateStudentInput} from "./create-student.input";
@@ -12,5 +12,17 @@ export class StudentResolver {
         @Args('createStudentInput') createStudentInput: CreateStudentInput
     ) {
         return this.studentService.createStudent(createStudentInput);
+    }
+
+    @Query(returns => [StudentType])
+    async getStudents() {
+        return this.studentService.getStudents()
+    }
+
+    @Query(returns => StudentType)
+    async getStudent(
+        @Args('id') id: string
+    ) {
+        return this.studentService.getStudent(id)
     }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -20,4 +20,16 @@ export class StudentService {
 
         return this.studentRepository.save(student)
     }
+
+    async getStudents(): Promise<Student[]> {
+        return this.studentRepository.find();
+    }
+
+    async getStudent(id: string): Promise<Student> {
+        return this.studentRepository.findOne({
+            where: {
+                id
+            }
+        })
+    }
 }
