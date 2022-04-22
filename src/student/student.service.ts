64e27915affdc775aa1fe0340abc935a588a938e
Mabrur <mabrur.h11@gmail.com>
2022-04-22 11:21:28 +0500

    update students getter

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Student} from "./student.entity";
-import {Repository} from "typeorm";
+import {In, Repository} from "typeorm";
 import {CreateStudentInput} from "./create-student.input";
 import { v4 as uuid } from 'uuid';
 
@@ -34,23 +34,10 @@ export class StudentService {
     }
 
     async getManyStudents(studentIds: string[]): Promise<Student[]> {
-        // return this.studentRepository.find({
-        //     where: {
-        //         id: {
-        //             $in: studentIds
-        //         }
-        //     }
-        // });
-
-        // I know this method isn't good for the performance, but the method in the above didn't work :(
-        let students = [];
-        for (let i = 0; i < studentIds.length; i++) {
-            students.push(this.studentRepository.findOne({
-                where: {
-                    id: studentIds[i]
-                }
-            }))
-        }
-        return students;
+        return this.studentRepository.find({
+            where: {
+                id: In(studentIds)
+            }
+        });
     }
 }
